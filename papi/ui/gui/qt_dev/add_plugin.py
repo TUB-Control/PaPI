59b5a8280ca1758d727d5a0e2dd4fe8294dd7cd7
Sven Knuth <Sven.C.Knuth@campus.tu-berlin.de>
2015-01-12 17:06:56 +0100

    Merge branch 'development' of https://github.com/TUB-Control/PaPI into development
    Conflicts:
    	papi/last_active_papi.xml
    
    added about message, changes in gui due to about action,

--- papi/ui/gui/qt_dev/add_plugin.py
+++ papi/ui/gui/qt_dev/add_plugin.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/gui/qt_dev/add_plugin.ui'
 #
-# Created: Mon Jan  5 14:03:51 2015
+# Created: Mon Jan 12 16:53:32 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
