a12c81a5d55f54e6704cba0e9cf0922cb7fb00e0
Sven Knuth <Sven.C.Knuth@campus.tu-berlin.de>
2015-01-15 11:51:16 +0100

    added documentation

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ release = ''
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'yapsy', 'pyqtgraph']
+exclude_patterns = ['_build', 'papi.yapsy', 'papi.pyqtgraph']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
@@ -107,7 +107,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
