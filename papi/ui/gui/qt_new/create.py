864bda1cd8c7f4519d0a94dfbda343c681188af2
StefanBerlin <rupp.stef@googlemail.com>
2015-04-14 15:50:43 +0200

    added possibility to start gui first

--- papi/ui/gui/qt_new/create.py
+++ papi/ui/gui/qt_new/create.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/gui/qt_new/create.ui'
 #
-# Created: Thu Apr  2 19:59:04 2015
+# Created: Tue Apr 14 15:50:34 2015
 #      by: pyside-uic 0.2.15 running on PySide 1.2.1
 #
 # WARNING! All changes made in this file will be lost!
