93121c67cd26a0b94019f6438fc9061933dbf740
Sven Knuth <sven.knuth@yawc.de>
2015-06-25 15:53:53 +0200

    Added an example for a PaPI simulink configuration based on the provided PackageFramework

---
+++ data_sources/Simulink/simulink_example_config.m
@@ -0,0 +1,70 @@
+function [ json_config ] = simulink_example_config( state, compact)
+%SIMULINK_EXAMPLE_CONFIG Summary of this function goes here
+%   Detailed explanation goes here
+    block_name_ortd = 'SourceGroup0';
+    plugin_name_ortd = 'ORTDPlugin1';
+
+        
+    if state == 1
+                             
+        signals = {'sine_1','sine_multiplier', 'sine_3', 'noise', 'pulse','sine','new_sine', 'current_state'};
+        parameters = {'multiplier','p2'};
+                       
+        pf = PacketFramework('PaPIConfig');
+
+        plot_uname = pf.PF_addplugin('Plot', 'PlotData', '(300,300)', '(0,0)');
+        slider_uname = pf.PF_addplugin('Slider', 'Multiplier', '(150,75)', '(300,0)');
+ 
+        pf.PF_changePluginConfig(slider_uname, { ...
+                {'upper_bound', '100'}, ...
+                {'lower_bound', '0'}, ...
+                {'step_count', '101'} ...
+            } ...
+        )
+        
+        %pf.PF_addparametersForBlockConfig({'Multiplier'});
+        %pf.PF_addparametersForBlockConfig({'Parameter(105)'});
+
+        
+        
+        % pf.PF_addsignalsForBlockConfig(signals(1:3));
+
+        % Create subscription
+        
+        pf.PF_addsubs(plot_uname, block_name_ortd, {signals(8)});
+        
+        pf.PF_addcontrol(slider_uname, 'SliderBlock', parameters(1));
+        
+        json_config = savejson('', pf.config, 'Compact', compact);
+    
+    end
+    
+    if state == 2
+
+        pf = PacketFramework('PaPIConfig');
+
+        plot_uname = pf.PF_addplugin('Plot', 'PlotData', '(300,300)', '(0,0)');
+        slider_uname = pf.PF_addplugin('Slider', 'Multiplier', '(150,75)', '(300,0)');
+ 
+        pf.PF_changePluginConfig(slider_uname, { ...
+                {'upper_bound', '100'}, ...
+                {'lower_bound', '0'}, ...
+                {'step_count', '101'} ...
+            } ...
+        )
+        
+
+        signals = {'sine_1','sine_multiplier', 'sine_3', 'noise', 'pulse','sine','new_sine', 'current_state'};
+        
+
+        % Create subscription
+        
+        pf.PF_addsubs(plot_uname, block_name_ortd, signals);
+        
+        pf.PF_addcontrol(slider_uname, 'SliderBlock', 'Multiplier');
+        
+        json_config = savejson('', pf.config, 'Compact', compact);
+    
+    end
+end
+
